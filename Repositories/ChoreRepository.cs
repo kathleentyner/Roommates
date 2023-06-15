42d4d8a9dbfd3d0387cb5aa99a186504ce501fd2
Kathleen Tyner <kathleentyner@gmail.com>
2023-06-15 11:48:40 -0400

    finished....

---
+++ Repositories/ChoreRepository.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+using Roommates.Models;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Roommates.Repositories
+
+{   
+    public class ChoreRepository : BaseRepository
+    {
+        
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+
+                conn.Open();
+
+
+                using (SqlCommand cmd = conn.CreateCommand()) //where I tell C# to create a new SQL query
+
+                {
+                    cmd.CommandText = "SELECT Id, Name from Chore"; //what is the SQL i want? you cab test it by doing a SQL query.
+
+                    SqlDataReader reader = cmd.ExecuteReader(); //read what I queried
+
+                    List<Chore> chores = new List<Chore>();
+
+                    while (reader.Read()) // loop through my sql command as long as there is more data
+                    {
+
+                        int idColumnPosition = reader.GetOrdinal("Id"); //getordinal means what column is it
+                        int idValue = reader.GetInt32(idColumnPosition); //what does the int say
+                        int nameColumnPosition = reader.GetOrdinal("Name");//what is the column position
+                        string nameValue = reader.GetString(nameColumnPosition); //what does the string say
+
+                        Chore chore = new Chore //new instance of a chore
+                        {//make a new object
+                            Id = idValue,
+                            Name = nameValue
+
+                        };
+                        //add the object to the list
+                        chores.Add(chore);
+
+                    }
+                    //close the reader
+                    reader.Close();
+
+                    return chores; //return the list of chores
+                }
+            }
+        }
+
+        ///  Returns a single chore with the given id.
+        public Chore GetById(int id)
+        {
+            using (SqlConnection connection = Connection)
+            {
+                connection.Open();
+                using (SqlCommand cmd = connection.CreateCommand())//build command
+                {
+                    cmd.CommandText = "Select Id, Name from Chore where Id = @id";//@ means we are using a parameter in the command we are building
+                    cmd.Parameters.AddWithValue("id", id);//We need to add a key/value pair to the parameter table in our command so it knows what we are talking about
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Chore chore = null;
+                        //Returning 1 object, don't need a loop.
+                        if (reader.Read())
+                        {
+                            chore = new Chore()
+                            {
+                                Id = id,
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+                        }
+
+                        return chore;
+                    }
+                }
+            }
+        }
+    
+            /// <summary>
+            ///  Add a new chore to the database
+            ///   NOTE: This method sends data to the database,
+            ///   it does not get anything from the database, so there is nothing to return.
+            /// </summary>
+
+           public void Insert(Chore chore)
+            {
+
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"insert into chore (Name) output inserted.Id values(@name)";
+                        cmd.Parameters.AddWithValue("@name", chore.Name);
+                        int id = (int)cmd.ExecuteScalar();
+
+                        chore.Id = id;
+                    }
+
+
+                }
+            }
+
+           
+        }
+
+    }
+
+//Add a method to ChoreRepository called GetUnassignedChores.
+//It should not accept any parameters and should return a list of chores that don't have any roommates already assigned to them.
+//After implementing this method, add an option to the menu so the user can see the list of unassigned chores.
---
+++ Repositories/RoommateRepository.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+using System.Data;
+
+namespace Roommates.Repositories
+{
+    public class RoommateRepository : BaseRepository
+    {
+        public RoommateRepository(string connectionString) : base(connectionString) { }
+
+        public List<Roommate> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT * FROM Roommate JOIN Room ON Roommate.RoomId = Room.Id";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Roommate> roommates = new List<Roommate>();
+
+                        while (reader.Read())
+                        {
+                            int idValue = reader.GetInt32(reader.GetOrdinal("Id"));
+                            string firstNameValue = reader.GetString(reader.GetOrdinal("FirstName"));
+                            string lastNameValue = reader.GetString(reader.GetOrdinal("LastName"));
+                            int rentPortionValue = reader.GetInt32(reader.GetOrdinal("RentPortion"));
+
+                            Room room = new Room
+                            {
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+
+                            Roommate roommate = new Roommate
+                            {
+                                Id = idValue,
+                                FirstName = firstNameValue,
+                                LastName = lastNameValue,
+                                RentPortion = rentPortionValue,
+                                Room = room
+                            };
+
+                            roommates.Add(roommate);
+                        }
+
+                        return roommates;
+                    }
+                }
+            }
+        }
+        public Roommate GetById(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT FirstName, RentPortion, r.Name as roomName" +
+                        "FROM Roommate " +
+                        "Join Room r on r.id = roommate.RoomId" +
+                        "WHERE Roommate.Id = @id";
+
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    Roommate roommate = null;
+
+                    // If we only expect a single row back from the database, we don't need a while loop.
+                    if (reader.Read())
+                    {
+                        roommate = new Roommate
+                        {
+                            Id = id,
+                            FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                           // LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            RentPortion = reader.GetInt32(reader.GetOrdinal("RentPortion")),
+                           // MovedInDate = reader.GetDateTime(reader.GetOrdinal("MoveInDate")),
+                           Room = new Room()
+                           {
+                               Name= reader.GetString(reader.GetOrdinal("")),
+
+
+
+                           }
+                        };
+                    }
+
+                    reader.Close();
+
+                    return roommate;
+                }
+            }
+        }
+
+    }
+}
